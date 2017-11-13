2f88fe0fe2c4c42f561cd40f9e0d2c79a09f788d
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-13 14:50:46 -0500

    Updated from Brython Server: 11/13/2017 2:50:44 PM

--- middleWord.py
+++ middleWord.py
@@ -6,7 +6,7 @@ words = input('Enter some words: ').split(' ')
 mid = len(words)//2
 
 if len(words)%2 == 0:
-        midWords = words[mid:mid+1]
+        midWords = words[mid:mid-1]
 else: 
         midWords = words[mid]
 
