4d163b9271143be5469a7cdc98312fdd5762e017
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-13 14:44:06 -0500

    Updated from Brython Server: 11/13/2017 2:44:05 PM

--- middleWord.py
+++ middleWord.py
@@ -3,8 +3,10 @@
 #middleWord.py - prints middle word
 
 words = input('Enter some words: ').split(' ')
-words.sort()
 
+if 
+middle = len(words)/2
+middleWords = words[middle:middle+2]
 for w in words:
     print(w)
     
