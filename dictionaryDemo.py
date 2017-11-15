93802362bb144d5f4809c354a6db5398c55551ca
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-15 13:33:50 -0500

    Updated from Brython Server: 11/15/2017 1:33:48 PM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,11 @@
+#Andrew Parker
+#11/15/17
+#dictionaryDemo.py - list practice 
 
+words = input('Enter some words: ').split(' ')
+
+dictionary = ['alphabet','sweatshirt','sweatpants','shorts','computer','waterbottle']
+
+dictionary.sort()
+
+number = int(input('What number word do you want to look up? '))
