9bd6f104e9ad93a90ebeb00b174c4a79648cd75f
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-15 13:51:45 -0500

    Updated from Brython Server: 11/15/2017 1:51:42 PM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,14 @@
+#Andrew Parker
+#11/15/17
+#longestWord.py - prints longest word
+
+words = input('Enter some words: ').split(' ')
+words.sort()
+
+if len(words)%2 == 0:
+        print(words[mid:mid+2])
+else: 
+        print(words[mid+1])
+
+
 
