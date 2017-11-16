10ab870a1c10069569f677c862c5b5555f1f5320
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-16 14:19:51 -0500

    Updated from Brython Server: 11/16/2017 2:19:50 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,22 @@
+#Andrew Parker
+#11/16/17
+#warmup13.py - makes list of random numbers and prints min max and sum
+
+from ggame import *
+from random import randint
+
+numbers = []
+for i in range(1,21):
+    numbers.append(randint)
+
+numbers.sort()
+
+print(numbers[0])
+print(numbers[20])
+print(numbers)
+    
+
+
+
+
 
