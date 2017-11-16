ff1a390aba5ba57c91eca4836648ed0f81e9d45f
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-16 14:54:27 -0500

    Updated from Brython Server: 11/16/2017 2:54:25 PM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -6,16 +6,8 @@ from ggame import *
 from random import randint
 randint (0,1)
 
-white = Color(0xFFFFFF,1)
-black = Color(0x000000,1)
-red = Color(0xFF0000,1) 
-yellow = Color(0xFFFF00,1)
-orange = Color(0xFFA500,1)
-green = Color(0x00FF00,1) 
-lightBlue = Color(0x00FFFF,1) 
-blue = Color(0x0000FF,1) 
-purple = Color(0x7F00FF,1) 
-darkPink = Color(0xFF007F,1) 
+Colors = [Color(0xFFFFFF,1), Color(0x000000,1), Color(0xFF0000,1), Color(0xFFFF00,1),Color(0xFFA500,1), Color(0x00FF00,1), Color(0x00FFFF,1), Color(0x0000FF,1), Color(0x7F00FF,1), Color(0xFF007F,1)] 
+#white = black = red =  yellow = orange = green =  lightBlue =  blue =  purple =  darkPink =  
 
 blackOutline = LineStyle(1,black) #pixels, color
 
