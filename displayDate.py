67fb6154213f8f8a36a657327786793dce9195f7
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-17 14:52:13 -0500

    Updated from Brython Server: 11/17/2017 2:52:13 PM

--- displayDate.py
+++ displayDate.py
@@ -1,6 +1,7 @@
 #Andrew Parker
 #11/17/17
-#warmup14.py - gives initials
+#displayDate.py - gives current date
 
-name = input('Enter your name? ').split(' ')
-print('Your initials are', name[0][0], 'and', name[1][0])
+from datetime import 
+
+today = datetime.date.today() 
