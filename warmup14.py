bd66b6ed4addcf982f42aa7c66ed1674e30b2ecc
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-17 14:41:58 -0500

    Updated from Brython Server: 11/17/2017 2:41:58 PM

--- warmup14.py
+++ warmup14.py
@@ -2,5 +2,5 @@
 #11/17/17
 #warmup14.py - gives initials
 
-name = [[input('Enter your name? ').split(' ')]]
-print('Your initials are', name[0][0], 'and', name[1][1])
+name = input('Enter your name? ').split(' ')
+print('Your initials are', name[0][0], 'and', name[1][0])
