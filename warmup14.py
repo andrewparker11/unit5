5c94bfee4defe325e01e9e5e915236e5403d5fb8
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-17 14:34:10 -0500

    Updated from Brython Server: 11/17/2017 2:34:10 PM

--- warmup14.py
+++ warmup14.py
@@ -1,1 +1,19 @@
+#Andrew Parker
+#11/17/17
+#matrixDemo.py - how to create/use a matrix
 
+def printBoard():
+    for row in range(0,3):
+        for col in range(0,3):
+            print(board[row][col],' ',end = '') #end is keyword
+        print()
+
+board = [['a','b','c'],['d','e','f'],['g','h','i']]
+printBoard()
+
+row = int(input('Enter a row number: '))
+col = int(input('Enter a col number: '))
+
+board[row][col] = 'X'
+
+printBoard()
