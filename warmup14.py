54a2a24427a335916c84526b6331d291bd8d4e1f
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-17 14:38:23 -0500

    Updated from Brython Server: 11/17/2017 2:38:23 PM

--- warmup14.py
+++ warmup14.py
@@ -1,19 +1,6 @@
 #Andrew Parker
 #11/17/17
-#matrixDemo.py - how to create/use a matrix
+#warmup14.py - gives initials
 
-def printBoard():
-    for row in range(0,3):
-        for col in range(0,3):
-            print(board[row][col],' ',end = '') #end is keyword
-        print()
-
-board = [['a','b','c'],['d','e','f'],['g','h','i']]
-printBoard()
-
-row = int(input('Enter a row number: '))
-col = int(input('Enter a col number: '))
-
-board[row][col] = 'X'
-
-printBoard()
+name = [input('Enter your name? ').split(' ')]
+print('Your initials are', name[0][0]
