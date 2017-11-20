af38fb8be018499f12ee9d2159922cfa855eee99
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-20 14:29:37 -0500

    Updated from Brython Server: 11/20/2017 2:29:36 PM

--- antsDemo.py
+++ antsDemo.py
@@ -8,6 +8,9 @@ from random import randint
 WIDTH = 800
 HEIGHT = 400
 
+def step():
+    for ant in data['antList']
+
 if __name__ == '__main__':
     
     red = Color(0xFF0000,1)
@@ -20,4 +23,4 @@ if __name__ == '__main__':
         data['antList'].append(Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT))))
 
     
-    App().run()
+    App().run(step)
