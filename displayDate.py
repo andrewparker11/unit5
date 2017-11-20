6b621e77275d08fb52ec4daaa74282dd3455edbb
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-20 14:54:39 -0500

    Updated from Brython Server: 11/20/2017 2:54:38 PM

--- displayDate.py
+++ displayDate.py
@@ -5,8 +5,22 @@
 from datetime import date
 
 today = date.today() 
-day = []
-print('Today is',)
-print(today.day)
+day = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+if today.weekday == 0:
+    day == 'Monday'
+elif today.weekday == 1:
+    day == 'Tuesday'
+elif today.weekday == 2:
+    day == 'Wednesday'
+elif today.weekday == 3:
+    day == 'Thursday'
+elif today.weekday == 4:
+    day == 'Friday'
+elif today.weekday == 5:
+    day == 'Saturday'
+elif today.weekday == 6:
+    day == 'Sunday'
+
+print(day)
 print(today.month)
 print(today.year)
