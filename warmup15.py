9f9cdf021660ffa04809bd647cb18f49154925eb
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-30 14:32:34 -0500

    Updated from Brython Server: 11/30/2017 2:32:34 PM

--- warmup15.py
+++ warmup15.py
@@ -3,9 +3,8 @@
 #warmup15.py - gets list of numbers and makes x2 list
 
 def doubled(x):
-    for x in x:
-        x[0] = 2*x[0]
-        x[1] = 2*x[1]
-        x[2] = 2*x[2]
-
+    y = []
+    for i in x:
+        y.append(2*i)
+    return y
 print(doubled([2,4,6]))
