cf0d3a2b9ebb1d504c1c74a9e6eca6b7a1a8f7b8
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-04 14:39:31 -0500

    Updated from Brython Server: 12/4/2017 2:39:30 PM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,15 @@
+#Andrew Parker
 
+#biggest
+
+def biggest(x):
+    y = []
+    for i in x:
+        if x[0]>x[1] and x[0]>x[2]:
+            biggest = x[0]
+        if x[1]>x[0] and x[1]>x[2]:
+            biggest = x[1]
+        if x[2]>x[1] and x[2]>x[0]:
+            biggest = x[2]
+    return biggest
+print(biggest([2,4,6]))
